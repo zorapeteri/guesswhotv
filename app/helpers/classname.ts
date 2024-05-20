65e0201cbfd96862cfebaa30e628ca335ca65202
Zora Péteri <zorapeteri@gmail.com>
2024-05-20 15:33:00 +0200

    sass deprecation fixes

--- app/helpers/classname.ts
+++ app/helpers/classname.ts
@@ -1,5 +1,5 @@
 export function classname(
   ...args: (string | number | undefined | null | false)[]
 ) {
-  return { className: args.filter(Boolean).join(" ") };
+  return { className: args.filter(Boolean).join(" ") }
 }
