84830672d69d181b74ca0acea7ad9067e50eb5e8
Zora Péteri <zorapeteri@gmail.com>
2024-05-20 21:06:16 +0200

    use client on search

--- app/routes/$search.tsx
+++ app/routes/$search.tsx
@@ -1,17 +1,43 @@
-import { type LoaderFunctionArgs, redirect } from '@remix-run/node'
-import slug from 'slug'
-import { searchShows } from '~/api/search'
+import { type LoaderFunctionArgs, redirect } from "@remix-run/node"
+import { ClientLoaderFunction } from "@remix-run/react"
+import slug from "slug"
+import { searchShows } from "~/api/search"
+import canUseJS from "~/helpers/canUseJS"
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const search = new URL(request.url).pathname.replace('/', '')
+  const url = new URL(request.url)
+  if (canUseJS(url)) {
+    return null
+  }
+
+  const search = url.pathname.replace("/", "")
+
   const shows = await searchShows(search)
   if (shows?.length) {
     return redirect(
-      `/show/${[slug(shows[0].show.name), shows[0].show.id].join('-')}`
+      `/show/${[slug(shows[0].show.name), shows[0].show.id].join("-")}`
     )
   }
-  throw new Response('404', { status: 404 })
+  throw new Response("404", { status: 404 })
 }
+
+export const clientLoader: ClientLoaderFunction = async ({ request }) => {
+  const search = new URL(request.url).pathname.replace("/", "")
+
+  const shows = await searchShows(search)
+
+  if (shows?.length) {
+    location.href =
+      location.origin +
+      `/show/${[slug(shows[0].show.name), shows[0].show.id].join("-")}`
+
+    return null
+  }
+
+  throw new Response("404", { status: 404 })
+}
+
+clientLoader.hydrate = true
 
 export default function Search() {
   return null
