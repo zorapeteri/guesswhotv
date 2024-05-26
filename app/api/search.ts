4cd612fc460383987fba5cf7ffb11ad149885812
Zora Péteri <zorapeteri@gmail.com>
2024-05-26 21:34:20 +0200

    extend forbidden terms

--- app/api/search.ts
+++ app/api/search.ts
@@ -1,6 +1,16 @@
-import apiPath from '~/constants/apiPath'
-import type { ShowResult } from '~/types/show'
+import apiPath from "~/constants/apiPath"
+import forbiddenGenres from "~/constants/forbiddenGenres"
+import type { ShowResult } from "~/types/show"
 
 export async function searchShows(query: string): Promise<ShowResult[]> {
-  return fetch(`${apiPath}/search/shows?q=${query}`).then((res) => res.json())
+  return fetch(`${apiPath}/search/shows?q=${query}`)
+    .then((res) => res.json())
+    .then((res) =>
+      res.filter(
+        (showResult: ShowResult) =>
+          !showResult.show.genres?.some((genre) =>
+            forbiddenGenres.includes(genre)
+          )
+      )
+    )
 }
